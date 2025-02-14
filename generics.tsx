4e16f3c6ca24997a1080e4f5ed10b3ab4cb97209
gbluv <greg.bluvshteyn@gmail.com>
2025-02-14 10:54:57 -0500

    Update generics.tsx

--- generics.tsx
+++ generics.tsx
@@ -10,6 +10,8 @@ const TextNode  = LinkNode<string> = {
   }
 }
 
+const stringNode = createLink('some-string');
+const numberNode = createLink(39)
 /*
 
   The <T,> in front of the function createLink is declaring a generic type parameter for the function. Let's break it down:
